84338d079d8303820abeeeeddb529f96b982365a
shehroz76 <shehroz076@gmail.com>
2016-11-12 21:59:00 -0800

    Frind req node added

--- app/src/main/java/com/ssuet/connect/ssuetconnect/UserFragment.java
+++ app/src/main/java/com/ssuet/connect/ssuetconnect/UserFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +16,14 @@ import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,8 +36,25 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class UserFragment extends Fragment {
 
 
+    private DatabaseReference mdatabaseFriendReqRef;
+    private DatabaseReference mdatabaseFriendPendingRef;
+    private DatabaseReference mdatabaseFriendsRef;
+    private DatabaseReference mDatabase;
+    private DatabaseReference mpendingref;
+    private String namePending;
+    private String batchPending;
+    private String namechehcking;
+    private String batchCheking;
+    private String UserPostion;
+
+    public UserFragment() {
+
+    }
 
     String UserUid;
+    String name;
+    String batch;
+    private boolean mFriend_Req = false;
 
     @Nullable
     @Override
@@ -43,9 +66,54 @@ public class UserFragment extends Fragment {
                 R.id.chat_lists_recylrview);
         rv.setHasFixedSize(true);
         rv.setLayoutManager(new LinearLayoutManager(getActivity()));
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
         FirebaseAuth mAuth =  FirebaseAuth.getInstance();
         UserUid = mAuth.getCurrentUser().getUid();
+
+
+
+        DatabaseReference mref=mDatabase.child(UserUid);
+        mref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                Map<String, String> map = (Map)dataSnapshot.getValue();
+                name = map.get("Name");
+                batch =map.get("Batch");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+        DatabaseReference mcheckingref=mDatabase.child(UserUid).child("Friend_Req");
+        mcheckingref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                Map<String, String> map = (Map)dataSnapshot.getValue();
+                namechehcking = map.get("Name");
+                batchCheking =map.get("batch");
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
 
 
 
@@ -61,7 +129,33 @@ public class UserFragment extends Fragment {
                     @Override
                     protected void populateViewHolder(final UserViewHolder viewHolder, UserModel model, int position) {
 
-                       final String UserPostion = getRef(position).getKey();
+                       UserPostion = getRef(position).getKey();
+
+
+                        mpendingref=mDatabase.child(UserPostion);
+                        mpendingref.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                                Map<String, String> map = (Map)dataSnapshot.getValue();
+                                namePending = map.get("Name");
+                                batchPending =map.get("Batch");
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+
+
+
+
+
 
                         viewHolder.setImage(getActivity(), model.getProfile_Image());
                         viewHolder.setTitle(model.getName());
@@ -72,14 +166,53 @@ public class UserFragment extends Fragment {
                         }
                         else
                         viewHolder.request.setImageResource(R.drawable.ic_action_user_add);
+                        
+                        
+                        
+                        
+                        
 
 
                         viewHolder.request.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
 
-                                viewHolder.request.setImageResource(R.drawable.request);
-                                FriendReuest();
+                                mFriend_Req = true;
+
+                                if(mFriend_Req) {
+
+                                if(UserUid.equals(UserPostion)){
+
+                                    viewHolder.request.setImageResource(R.drawable.current_user);
+                                }else
+
+
+                                    mdatabaseFriendReqRef = FirebaseDatabase.getInstance().getReference().child("Users").child(UserPostion).child("Friend_Req");
+
+                                    mdatabaseFriendReqRef.child(UserUid).child("Name").setValue(name);
+                                    mdatabaseFriendReqRef.child(UserUid).child("batch").setValue(batch);
+
+
+
+                                    mdatabaseFriendPendingRef = FirebaseDatabase.getInstance().getReference().child("Users").child(UserUid).child("Friends_Pending");
+
+
+                                    mdatabaseFriendPendingRef.child(UserPostion).child("Name").setValue(namePending);
+                                    mdatabaseFriendPendingRef.child(UserPostion).child("batch").setValue(batchPending);
+
+                                    if(!UserUid.equals(UserPostion)) {
+
+
+                                        viewHolder.request.setImageResource(R.drawable.request);
+                                    }
+
+
+
+                                    mFriend_Req =false;
+
+                                }
+
+
 
 
                             }
